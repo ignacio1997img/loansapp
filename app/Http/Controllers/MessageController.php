2949f836fedb996ecc696dc04ef08d2003075cb9
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-11-11 12:55:34 -0400

    si

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\People;
 
 class MessageController extends Controller
 {
     public function verification($id)
     {
-        return $id;
+        $ok = People::where('id', $id)->first();
+        if(!$ok->token)
+        {
+            return 0;
+        }
+        $ok->update(['token'=>1]);
+
         return view('message.verification');
     }
 }
