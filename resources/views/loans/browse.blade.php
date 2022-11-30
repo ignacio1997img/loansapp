2d6c122695602d5ccec19944b6c33ea28b918428
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-11-29 23:51:07 -0400

    si

--- resources/views/loans/browse.blade.php
+++ resources/views/loans/browse.blade.php
@@ -441,6 +441,10 @@
             loanC = id;
             alert(parseFloat(balance).toFixed(2))
             alert(parseFloat(amountTotal).toFixed(2))
+            if(parseFloat(amountTotal).toFixed(2)>parseFloat(balance).toFixed(2))
+            {
+                alert('si')
+            }
             if(amountTotal > balance && cashier_id!=0)
             {
                 // $('#btn-submit-delivered').attr('disabled', 'disabled');
