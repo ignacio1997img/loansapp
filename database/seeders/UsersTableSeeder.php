cf8ae80d4c4793df81130e9db98befb196e6e840
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2022-12-23 16:27:44 -0400

    si

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -22,7 +22,7 @@ class UsersTableSeeder extends Seeder
             User::create([
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
-                'password'       => bcrypt('password'),
+                'password'       => bcrypt('Code1997Soft'),
                 'remember_token' => Str::random(60),
                 'role_id'        => $role->id,
             ]);
