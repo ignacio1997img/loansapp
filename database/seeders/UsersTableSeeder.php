5adec5f2841902d52f8d9686753cca973f63ef3b
ignacio1997img+ <ignaciomolinaguzman20@gmail.com>
2023-01-06 14:38:30 -0400

    inicio del sistema a funcionamiento

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -29,7 +29,7 @@ class UsersTableSeeder extends Seeder
             User::create([
                 'name'           => 'Gerente',
                 'email'          => 'gerente@admin.com',
-                'password'       => bcrypt('*gerente.,*2022*'),
+                'password'       => bcrypt('12345678.,*'),
                 'remember_token' => Str::random(60),
                 'role_id'        => 2,
             ]);
