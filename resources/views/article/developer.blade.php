c3197bfbc282d851b5700f904735090c3cd8ea6b
ignacio1997img <ignaciomolinaguzman20@gmail.com>
2023-07-14 20:10:49 -0400

    si

--- resources/views/article/developer.blade.php
+++ resources/views/article/developer.blade.php
@@ -2,7 +2,7 @@
 
 @section('page_title', 'Crear Input')
 
-@if (auth()->user()->hasPermission('input_articles'))
+{{-- @if (auth()->user()->hasPermission('input_articles')) --}}
 
     @section('page_header')
         <h1 id="titleHead" class="page-title">
@@ -247,4 +247,4 @@
         </script>
     @stop
 
-@endif
+{{-- @endif --}}
