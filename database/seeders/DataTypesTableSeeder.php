174236a7bf5d6da79a96ec3ab2d3ba8c8d53c1ab
ignaciomolinaguzman20@gmail.com <ignaciomolinaguzman20@gmail.com>
2023-07-28 11:50:54 -0400

    si

--- database/seeders/DataTypesTableSeeder.php
+++ database/seeders/DataTypesTableSeeder.php
@@ -305,7 +305,7 @@ class DataTypesTableSeeder extends Seeder
                 'server_side' => 0,
                 'details' => '{"order_column":null,"order_display_column":null,"order_direction":"asc","default_search_key":null,"scope":null}',
                 'created_at' => '2023-07-13 19:40:38',
-                'updated_at' => '2023-07-28 11:29:28',
+                'updated_at' => '2023-07-28 11:49:46',
             ),
             16 => 
             array (
